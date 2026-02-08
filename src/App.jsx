77b44280b5871dcc98143ab8e4d353bb5fc3f8be
harshmalu9 <hmalu975@gmail.com>
2026-02-08 20:09:47 +0530

    created the resizable window layoutfor terminal

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './app.scss'
 import Dock from "./components/Dock.jsx";
 import Nav from './components/Nav.jsx';
+import MacWindow from './components/windows/MacWindow.jsx';
 
 const App = () => {
   return (
     <main>
       <Nav />
+      <MacWindow >  </MacWindow>
       <Dock />
     </main>
   )
