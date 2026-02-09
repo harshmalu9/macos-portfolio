fabd88c1df45f656e1dbcd8f0ca1525ffd1be2f4
harshmalu9 <hmalu975@gmail.com>
2026-02-09 19:21:00 +0530

    used macWindow for making github

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './app.scss'
 import Dock from "./components/Dock.jsx";
 import Nav from './components/Nav.jsx';
 import MacWindow from './components/windows/MacWindow.jsx';
+import Github from './components/windows/Github.jsx';
 
 const App = () => {
   return (
     <main>
       <Nav />
-      <MacWindow >  </MacWindow>
+      <Github />
       <Dock />
     </main>
   )
